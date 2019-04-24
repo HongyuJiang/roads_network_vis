03d952ef02d0d9c80dcb116c0de640255d67e389
Hongyu Jiang <1230223112@stu.cjlu.edu.cn>
2019-04-24 11:24:19 +0800

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 
 import {StaticMap} from 'react-map-gl';
-import DeckGL, {LineLayer, ScatterplotLayer} from 'deck.gl';
+import DeckGL, {LineLayer, TripsLayer} from 'deck.gl';
 import GL from '@luma.gl/constants';
 import DataProvider from './DataProvider';
 import * as dsv from 'd3-dsv';
@@ -19,6 +19,8 @@ export const INITIAL_VIEW_STATE = {
   pitch: 50,
   bearing: 0
 };
+
+var road_locations_dict = {}
 
 // 解析数据
 function path_handle(data){
@@ -38,9 +40,49 @@ function path_handle(data){
     let meta = {'start':startPoints,'end':endPoints,'name':name}
 
     new_data.push(meta)
+
+    road_locations_dict[d.link_id] = startPoints
   })
 
   return new_data
+}
+
+function path_segments_constructor(data){
+
+  let trips_data = []
+
+  let taxis_segments_bukets = {}
+
+  data = dsv.csvParse(data);
+
+  data.forEach(function(d){
+
+    let coor =  road_locations_dict[d.road_id]
+    coor[2] = 1000 * parseInt(d.round) 
+    //console.log(parseInt(d.round) )
+    let occupation = d.occipation
+
+    if(taxis_segments_bukets[d.taxis_id] == undefined){
+
+      taxis_segments_bukets[d.taxis_id] = []
+      taxis_segments_bukets[d.taxis_id].push(coor)
+    }
+    else{
+
+      taxis_segments_bukets[d.taxis_id].push(coor)
+    }
+  })
+
+  for (let taxi in taxis_segments_bukets){
+
+    let meta = {'segments':taxis_segments_bukets[taxi]}
+
+    trips_data.push(meta)
+  }
+
+  console.log(trips_data)
+
+  return trips_data
 }
 
 export class App extends Component {
@@ -48,8 +90,10 @@ export class App extends Component {
     super(props);
 
     this.state = {
+      time: 0,
       hoveredObject: null,
-      linksData:{}
+      linksData:{},
+      tripsData:{}
     };
     this._onHover = this._onHover.bind(this);
     this._renderTooltip = this._renderTooltip.bind(this);
@@ -63,9 +107,45 @@ export class App extends Component {
 
         that.setState({linksData: data})
 
+        DataProvider.getTaxisPath().then(response => {
+      
+          let data = path_segments_constructor(response.data)
+  
+          that.setState({tripsData: data})
+  
+          }, error => {
+        
+      });
+
         }, error => {
       
     });
+  }
+
+  componentDidMount() {
+    this._animate();
+  }
+
+  componentWillUnmount() {
+    if (this._animationFrame) {
+      window.cancelAnimationFrame(this._animationFrame);
+    }
+  }
+
+  _animate() {
+
+    //console.log(this.state.time)
+    const {
+      loopLength = 400 * 1000, // unit corresponds to the timestamp in source data
+      animationSpeed = 30 // unit time per second
+    } = this.props;
+    const timestamp = Date.now() / 1000;
+    const loopTime = loopLength / animationSpeed;
+
+    this.setState({
+      time: ((timestamp % loopTime) / loopTime) * loopLength
+    });
+    this._animationFrame = window.requestAnimationFrame(this._animate.bind(this));
   }
 
   //鼠标悬停
@@ -94,8 +174,21 @@ export class App extends Component {
     } = this.props;
 
     const roads = this.state.linksData
+    const trips = this.state.tripsData
+    const trailLength = 180
 
     return [
+      new TripsLayer({
+        id: 'trips',
+        data: trips,
+        getPath: d => d.segments,
+        getColor: [23, 184, 190],
+        opacity: 0.3,
+        widthMinPixels: 2,
+        rounded: true,
+        trailLength,
+        currentTime: this.state.time
+      }),
       //绘制管线
       new LineLayer({
         id: 'road-paths',
